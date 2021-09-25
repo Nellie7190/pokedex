8e245ea82eccea2052f759b25a2669e6c4ce9a92
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-24 23:57:10 -0400

    init repo, index and show started

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//DATA
+const allPokes = require('./models/pokedex');
+
+//INDEX
+app.get('/allPokemon', (req, res) => {
+    res.render('index.ejs', {pokemon: allPokes})
+})
+
+//SHOW
+app.get('/allPokemon/:idx', (req, res) => {
+    res.render('show.ejs', {allPoke: allPokes[req.params.idx]})
+})
+
+
+app.listen(3000, () => {
+    console.log('listening on port:', port)
+})
