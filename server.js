27ae5244384b8cafbae721c90cafd2ee929fb72a
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-25 17:09:37 -0400

    created buttons edit,delete,create new that go nowhere

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const port = 3000;
 
 //DATA
 const allPokes = require('./models/pokedex');
 
+//MIDDLEWARE====================================
+app.use((req,res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+//Processes data and creates req.body
+app.use(express.urlencoded({extended:false}));
+
+//used to 'change' POST to DELETE
+app.use(methodOverride('_method'));
+
+/////ROUTES======================================
 //INDEX
 app.get('/allPokemon', (req, res) => {
-    res.render('index.ejs', {pokemon: allPokes})
+    res.render('index.ejs', {pokemon: allPokes});
+});
+
+//NEW
+app.get('/allPokemon/new', (req, res) => {
+    res.render('new.ejs');
 })
+
+//DELETE
+app.delete('/allPokemon/:id', (req, res) => {
+    allPokes.splice(req.params.idx, 1);
+    res.redirect('/allPokemon');
+});
 
 //SHOW
 app.get('/allPokemon/:idx', (req, res) => {
-    res.render('show.ejs', {allPoke: allPokes[req.params.idx]})
-})
+    res.render('show.ejs', {allPoke: allPokes[req.params.idx]});
+});
+
 
 
 app.listen(3000, () => {
-    console.log('listening on port:', port)
-})
-
-
+    console.log('listening on port:', port);
+});
 
